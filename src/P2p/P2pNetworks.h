5c039b5f207bddecb9beb64195b02e3d6df05727
DepositXFoundation <locmanmobile@gmail.com>
2018-04-05 14:33:22 +0300

    First initial commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x21, 0x42, 0x51, 0x30, 0x12, 0x1f, 0x3e, 0x2f, 0x51, 0x33, 0x00, 0x11, 0x11, 0x11, 0x35, 0x17 } };
+}
